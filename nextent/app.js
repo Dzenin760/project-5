51917791fd0fbf0020e60790579dc1614bb35f29
Dzenin760 <tursunoviles91@gmail.com>
2024-08-01 10:04:36 +0500

    Add files via upload

---
+++ nextent/app.js
@@ -0,0 +1,32 @@
+let plus=document.querySelector('.plus') 
+let son=document.querySelector('.son') 
+let conut=2245343 
+plus.addEventListener('click',()=>{ 
+    conut+=1; 
+    son.innerHTML=conut 
+} 
+) 
+let plus2=document.querySelector('.plus2') 
+let son2=document.querySelector('.son2') 
+let conut2=46328 
+plus2.addEventListener('click',()=>{ 
+    conut2+=1; 
+    son2.innerHTML=conut2
+} 
+) 
+let plus3=document.querySelector('.plus3') 
+let son3=document.querySelector('.son3') 
+let conut3=828867
+plus3.addEventListener('click',()=>{ 
+    conut3+=1; 
+    son3.innerHTML=conut3 
+} 
+) 
+let plus4=document.querySelector('.plus4') 
+let son4=document.querySelector('.son4') 
+let conut4=1926436 
+plus4.addEventListener('click',()=>{ 
+    conut4+=1; 
+    son4.innerHTML=conut4 
+} 
+) 
